a7329c67f506069af09cec030206b0b6d378af93
Awf Wiswasi <55606355+awiswasi@users.noreply.github.com>
2022-07-21 18:40:50 -0700

    Added necessary headers and fixed formatting.

--- include/state.hpp
+++ include/state.hpp
@@ -1,17 +1,20 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
 #include "state_engine.hpp"
 
 namespace mobius
 {
 
-class state
-{
-public:
-  virtual void enter() = 0;
-  virtual void handle_input(int ch) = 0;
-  virtual void restore() = 0;
-  virtual void exit() = 0;
-};
+  class state
+  {
+  public:
+    virtual void enter() = 0;
+    virtual void handle_input(int ch) = 0;
+    virtual void restore() = 0;
+    virtual void exit() = 0;
+  };
 
 } // namespace mobius
